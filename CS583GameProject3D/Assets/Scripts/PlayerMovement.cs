c1b65bce9b8611c7f60af376afe172ce3692247d
LuAlvaz <lalvarez5220@sdsu.edu>
2026-04-16 23:12:33 -0700

    level1 start
    I added a player model with movement to Level 1

---
+++ CS583GameProject3D/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 5f;
+
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveX, 0f, moveZ) * speed;
+
+        Vector3 velocity = rb.linearVelocity;
+        velocity.x = movement.x;
+        velocity.z = movement.z;
+        rb.linearVelocity = velocity;
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
