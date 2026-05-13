8950c2ba69a45b1dbb83a17801ae2935026543ad
LuAlvaz <lalvarez5220@sdsu.edu>
2026-05-13 03:16:27 -0700

    fixed player and scenes now conected

--- CS583GameProject3D/Assets/Scripts/Level2/PlayerDeathHandler.cs
+++ CS583GameProject3D/Assets/Scripts/Level2/PlayerDeathHandler.cs
@@ -1,19 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Collections;
 
 public class PlayerDeathHandler : MonoBehaviour
 {
     public void Die()
     {
-        StartCoroutine(RestartScene());
-    }
-
-    IEnumerator RestartScene()
-    {
         Time.timeScale = 1f;
-
-        yield return new WaitForSeconds(0.2f);
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
